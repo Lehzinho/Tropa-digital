23151f0b70f9734e0410e92d4c4d8e35d44f5fdb
Alexandre Toulios <99737712+Lehzinho@users.noreply.github.com>
2025-06-04 17:07:29 -0300

    cofig: instalation of  and  apply types to styled-component default theme

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import { DefaultTheme } from "../styles/themes/defaults";
+import "styled-components";
+
+type ThemeType = typeof DefaultTheme;
+
+declare module "styled-components" {
+  export interface DefaultTheme extends ThemeType {}
+}
